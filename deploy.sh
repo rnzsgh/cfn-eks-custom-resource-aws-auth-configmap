c980161e67c4d53c699ae9d780799bcea2343b82
Ryan <ryannitz@amazon.com>
2019-05-03 13:46:30 -0400

    Add public read to item on deploy

--- deploy.sh
+++ deploy.sh
@@ -8,3 +8,5 @@ GOOS=linux go build main.go auth.go
 zip cfn-eks-custom-resource-configmap.zip ./main
 
 aws s3 cp cfn-eks-custom-resource-configmap.zip s3://$BUCKET_NAME/cfn-eks-custom-resource-configmap.zip
+
+aws s3api put-object-acl --bucket $BUCKET_NAME -name --key cfn-eks-custom-resource-configmap.zip --acl public-readc
