ff04f007a827fd59c1563a2497f6b1fdb7707277
Ryan <ryannitz@amazon.com>
2019-06-25 12:39:22 -0400

    Deploy globally

--- deploy.sh
+++ deploy.sh
@@ -2,15 +2,25 @@
 
 rm -Rf cfn-eks-custom-resource-aws-auth-configmap.zip main
 
-BUCKET_NAME=public-aws-serverless-repo
+BUCKET_PREFIX=pub-cfn-cust-res-pocs
+
 GOOS=linux go build main.go auth.go
 
 zip cfn-eks-custom-resource-aws-auth-configmap.zip ./main
 
-aws s3 cp cfn-eks-custom-resource-aws-auth-configmap.zip s3://$BUCKET_NAME/cfn-eks-custom-resource-aws-auth-configmap.zip
+REGIONS=$(aws ec2 describe-regions --output text --query Regions[*].RegionName)
 
-aws s3api put-object-tagging --bucket $BUCKET_NAME --key cfn-eks-custom-resource-aws-auth-configmap.zip \
-  --tagging 'TagSet={Key=public,Value=yes}'
+# Currently, StackSets are not supported in these regions. This may change over time.
+REGIONS=${REGIONS//eu-north-1/}
+REGIONS=${REGIONS//ap-northeast-3/}
+
+for REGION in $REGIONS; do
+  aws s3 cp cfn-eks-custom-resource-aws-auth-configmap.zip \
+    s3://$BUCKET_PREFIX-$REGION/cfn-eks-custom-resource-aws-auth-configmap.zip --region $REGION
+
+  aws s3api put-object-tagging --region $REGION --bucket $BUCKET_PREFIX-$REGION --key cfn-eks-custom-resource-aws-auth-configmap.zip \
+    --tagging 'TagSet={Key=public,Value=yes}'
+done
 
 rm -Rf cfn-eks-custom-resource-aws-auth-configmap.zip main cfn-eks-custom-resource-aws-auth-configmap
 
